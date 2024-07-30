d198975df670460ff85d7924d5b1ad400b6782bb
fahima <sfahima1388@gmail.com>
2024-07-30 20:16:58 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlack: "#12141D",
+        primaryRed: "#FF664F",
+        primaryGreen: "#04AA69",
+        primaryViolet: "#855AFF",
+        primaryWhite: "#FFFFFF",
+        primaryGray: "#696B76",
+      },
+    },
+  },
+  plugins: [],
+};
